eb94da8cfb68670fa37cdfe9e41a2eba400f6b7e
I S Sarath <55507288+sarathithikkatt@users.noreply.github.com>
2023-10-23 01:49:11 +0530

    wip: added html viewer

---
+++ lib/render.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+class RenderWidget extends StatefulWidget {
+  var renderText;
+
+  RenderWidget({
+    super.key,
+    required this.renderText,
+  });
+
+  @override
+  State<RenderWidget> createState() => _RenderWidgetState();
+}
+
+class _RenderWidgetState extends State<RenderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    var renderText = widget.renderText;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          height: height / 2.4,
+          width: width / 2.5,
+          child: HtmlWidget(renderText)),
+    );
+  }
+}
